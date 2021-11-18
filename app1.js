23c5246f1e45c816cb07e3c9ff1ede5c058c66a9
regan hayes <hayesregan@yahoo.com>
2021-11-18 15:26:19 -0500

    added functions to js, edited css for nav buttons

--- app1.js
+++ app1.js
@@ -1,9 +1,13 @@
-let email = prompt('Enter your Email Address to subscribe!');
+function askUserEmail(){
+    let email = prompt('Enter your Email Address to subscribe!');
 
-console.log(email);
-//             'Welcome ' + 'Rae'
-if(email== 'hayesregan@yahoo.com'){
-    document.write('Welcome Chef ' + email)
-} else {
-    document.write('Welcome Foodie' + email)
+    console.log(email);
+    //         'Welcome ' + 'Rae'
+    if(email== 'hayesregan@yahoo.com'){
+        document.write('Welcome Chef ' + email)
+    } else {
+        document.write('Welcome Foodie' + email)
+    }
 }
+
+askUserEmail ()
---
+++ app4.js
@@ -0,0 +1,6 @@
+function addTwoNumbers(num1, num2)
+    let total = num1 + num2
+        console.log(total);
+}
+
+// addTwoNumbers (5, 10)
